2a213c99d067e8b3a6c842513ff1f6f356731c0d
Romanov5000 <romanovvladislav97@gmail.com>
2020-04-14 20:55:18 +0300

    ht_03

---
+++ 03/ht/Romanov5000/isDeepEqual.js
@@ -0,0 +1,3 @@
+let a = prompt('a','')
+let b = prompt('b','')
+alert(a===b)
---
+++ 03/ht/Romanov5000/isPalindrom.js
@@ -0,0 +1,9 @@
+let str = prompt('fff','');
+
+  strReverse = str.split('').reverse().join(''); 
+  if ( str === strReverse){
+    alert ('true');
+  } else {
+    alert ('false');
+  }
+  
