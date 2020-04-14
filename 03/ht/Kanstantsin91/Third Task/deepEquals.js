9362baffadb29c1f4de395a2f24dc987881e35f9
Kanstantsin Sakavets <kanstantsin.sakavets@gmail.com>
2020-04-14 19:06:06 +0300

    mistake fixed

--- 03/ht/Kanstantsin91/Third Task/deepEquals.js
+++ 03/ht/Kanstantsin91/Third Task/deepEquals.js
@@ -12,15 +12,15 @@ function deepEquals(variableOne, variableTwo) {
                 if (typeof variable[key] == 'function') {
                     objSrt = (objSrt + key + variable[key]).replace(/\s/g, '');
                 } else if (typeof variable[key] == 'string') {
-                    objSrt = (objSrt + key + '\'' + variable[key] + '\''), '');
-            } else if (typeof variable[key] != 'object' || variable[key] == null) {
-                objSrt = objSrt + key + variable[key];
-            } else objSrt = objSrt + variableToString(variable[key]);
-        }
-        return objSrt;
-    } else if (typeof variable == 'function') {
-        return (objSrt + variable).replace(/\s/g, '');
-    } else return objSrt + variable;
-}
-return variableToString(variableOne) === variableToString(variableTwo);
+                    objSrt = (objSrt + key + '\'' + variable[key] + '\'');
+                } else if (typeof variable[key] != 'object' || variable[key] == null) {
+                    objSrt = objSrt + key + variable[key];
+                } else objSrt = objSrt + variableToString(variable[key]);
+            }
+            return objSrt;
+        } else if (typeof variable == 'function') {
+            return (objSrt + variable).replace(/\s/g, '');
+        } else return objSrt + variable;
+    }
+    return variableToString(variableOne) === variableToString(variableTwo);
 }
